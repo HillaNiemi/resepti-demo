240b648cfb41a1a4326b919e47117f5da5833d87
HillaNiemi <hilla.e.niemi@gmail.com>
2022-04-01 19:40:06 +0300

    index, script, style

---
+++ public/script.js
@@ -0,0 +1,13 @@
+const searchBtn = document.getElementById('search-btn');
+const mealList = document.getElementById('meal');
+const mealDetailsContent = document.querySelector('meal-details-content')
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
+
+//event listeners
+searchBtn.addEventListener('click', getMealList);
+
+//get meal list that matches with the ingredients
+function getMealList(){
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    console.log(searchInputTxt);
+}
